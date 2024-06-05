c469a23f821dea3ad661882b39465f047f128d37
Hamisakim <76621344+Hamisakim@users.noreply.github.com>
2024-06-05 19:27:18 +0100

    chore: add basic tests

---
+++ test/utils/formatDateTime.test.ts
@@ -0,0 +1,20 @@
+import { formatDateTimeFromUTC } from '../../src/utils/formatDateTime';
+import { describe, expect, it } from 'vitest';
+
+describe('formatDateTime', () => {
+  it('should format the timestamp with custom options', () => {
+    const timestamp = '2022-01-01T12:34:56';
+    const options = { timeZone: 'America/New_York', hour12: true };
+    const formattedDateTime = formatDateTimeFromUTC(timestamp, options);
+
+    expect(formattedDateTime).toBe('January 1, 2022 at 7:34:56 AM EST');
+  });
+
+  it('should format the timestamp with no options', () => {
+    const timestamp = '2022-01-01T12:34:56';
+    const options = {};
+    const formattedDateTime = formatDateTimeFromUTC(timestamp, options);
+
+    expect(formattedDateTime).toBe('January 1, 2022 at 12:34:56 PM GMT');
+  });
+});
