ae61e3640d90ff22fdf226bdcc5b611dd584a984
Hamisakim <76621344+Hamisakim@users.noreply.github.com>
2024-06-05 19:10:24 +0100

    fix: shows locale time for single event

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -22,16 +22,15 @@ import { type Venue } from './Events';
 interface EventInfoProps {
   event: {
     short_title: string;
-    datetime_utc: Date;
+    datetime_utc: string;
     venue: Venue;
     url: string;
-  }
+  };
 }
 
 const Event: React.FC = () => {
   const { eventId } = useParams();
   const { data: event, error } = useSeatGeek(`events/${eventId}`);
-
   if (error) return <Error />;
 
   if (!event) {
@@ -39,19 +38,19 @@ const Event: React.FC = () => {
       <Flex justifyContent="center" alignItems="center" minHeight="50vh">
         <Spinner size="lg" />
       </Flex>
-    )
+    );
   }
 
   return (
     <>
-      <Breadcrumbs 
+      <Breadcrumbs
         items={[
           { label: 'Home', to: '/' },
           { label: 'Events', to: '/events' },
           { label: event.short_title },
-        ]} 
+        ]}
       />
-      <Flex bgColor='gray.200' p={[4, 6]}>
+      <Flex bgColor="gray.200" p={[4, 6]}>
         <Heading>{event.short_title}</Heading>
       </Flex>
       <EventInfo event={event} />
@@ -59,32 +58,41 @@ const Event: React.FC = () => {
   );
 };
 
-const EventInfo: React.FC<EventInfoProps> = ({ event }) => (
-  <Stack spacing="6" m="6">
-    <SimpleGrid 
-      columns={[1, 1, 2]} 
-      borderWidth="1px" 
-      borderRadius="md"
-      p="4" 
-    >
-      <Stat>
-        <StatLabel display="flex">
-          <Box as="span">Venue</Box>
-        </StatLabel>
-        <StatNumber fontSize="xl">{event.venue.name_v2}</StatNumber>
-        <StatHelpText>{event.venue.display_location}</StatHelpText>
-      </Stat>
-      <Stat>
-        <StatLabel display="flex">
-          <Box as="span">Date</Box>
-        </StatLabel>
-        <StatNumber fontSize="xl">{formatDateTime(event.datetime_utc)}</StatNumber>
-      </Stat>
-    </SimpleGrid>
-    <Flex>
-      <Button as={'a'} href={event.url} minWidth="0">Buy Tickets</Button>
-    </Flex>
-  </Stack>
-);
+const EventInfo: React.FC<EventInfoProps> = ({ event }) => {
+  //Personally prefer destructuring the props in the function for better readability at a negeligble cost of performance
+  //Saying that if the rest of the app is using the same pattern, it's better to stick with that, just what I prefer
+
+  const { datetime_utc } = event;
+  const { timezone } = event.venue;
+  
+  const utcDate = new Date(`${datetime_utc}Z`); //? Need to append Z so that the date is treated as UTC by code
+
+  return (
+    <Stack spacing="6" m="6">
+      <SimpleGrid columns={[1, 1, 2]} borderWidth="1px" borderRadius="md" p="4">
+        <Stat>
+          <StatLabel display="flex">
+            <Box as="span">Venue</Box>
+          </StatLabel>
+          <StatNumber fontSize="xl">{event.venue.name_v2}</StatNumber>
+          <StatHelpText>{event.venue.display_location}</StatHelpText>
+        </Stat>
+        <Stat>
+          <StatLabel display="flex">
+            <Box as="span">Date</Box>
+          </StatLabel>
+          <StatNumber fontSize="xl">
+            {formatDateTime(utcDate, { timeZone: timezone })}
+          </StatNumber>
+        </Stat>
+      </SimpleGrid>
+      <Flex>
+        <Button as={'a'} href={event.url} minWidth="0">
+          Buy Tickets
+        </Button>
+      </Flex>
+    </Stack>
+  );
+};
 
 export default Event;
--- src/utils/formatDateTime.ts
+++ src/utils/formatDateTime.ts
@@ -1,5 +1,11 @@
-export function formatDateTime(timestamp: Date) {
-  return new Intl.DateTimeFormat('en-US', {
+/**
+ * Formats a given timestamp into a localized date and time string.
+ * @param timestamp - The timestamp to format.
+ * @param options - Optional configuration options for formatting.
+ * @returns The formatted date and time string.
+ */
+export function formatDateTime(timestamp: Date, options?: Intl.DateTimeFormatOptions) {
+    const defaultOptions: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
@@ -7,5 +13,11 @@ export function formatDateTime(timestamp: Date) {
     minute: 'numeric',
     second: 'numeric',
     timeZoneName: 'short',
-  }).format(new Date(timestamp));
+    timeZone: 'America/New_York',
+    
+  };
+
+  const mergedOptions = { ...defaultOptions, ...options };
+
+  return new Intl.DateTimeFormat('en-US', mergedOptions,).format(new Date(timestamp));
 }
