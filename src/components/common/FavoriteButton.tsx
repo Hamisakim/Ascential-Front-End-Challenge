5f864f163969d1c63b80b0e9f2e31f73f2b89b74
Hamisakim <76621344+Hamisakim@users.noreply.github.com>
2024-06-06 11:43:56 +0100

    doc: favourite buttn ts-docs

--- src/components/common/FavoriteButton.tsx
+++ src/components/common/FavoriteButton.tsx
@@ -9,6 +9,16 @@ interface FavoriteButtonProps {
   name: string;
 }
 
+/**
+ * Renders a reusable cross component favorite button component.
+ *
+ * @component
+ * @param {FavoriteButtonProps} props - The component props.
+ * @param {string} props.id - The ID of the item.
+ * @param {string} props.type - The type of the item.
+ * @param {string} props.name - The name of the item.
+ * @returns {JSX.Element} The rendered FavoriteButton component.
+ */
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ id, type, name }) => {
   const { favorites, addFavorite, removeFavorite } = useFavorites();
   const isFavorite = favorites.some((fav) => fav.id === id);
